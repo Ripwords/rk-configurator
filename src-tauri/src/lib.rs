dbc40d862b1de055f419bb34f54d1330bb2784a7
JJ <teohjjteoh@gmail.com>
2025-12-28 23:34:58 +0800

    chore: add tauri-plugin-process to dependencies
    - Included tauri-plugin-process in package.json and Cargo.toml to enhance process management capabilities.
    - Updated lib.rs to initialize the new plugin for improved functionality.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -25,6 +25,7 @@ pub fn run() {
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_process::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
                 .add_migrations(
